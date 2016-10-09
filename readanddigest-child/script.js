98607e231e6c06eb3d327d0164af2d8e8f37c4a7
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-10-09 09:37:06 -0500

    Reorganizing file structure and footer updates

---
+++ readanddigest-child/script.js
@@ -0,0 +1,24 @@
+// Wordpress uses the 'jQuery' object by default to call jQuery.
+//  This wrapping, self-executing function allows us to use the
+//  default $ for all of our jQuery calls.
+(function ($) {
+  $(document).ready(function(){
+    // Changing the link of the logo to the UWO home page
+    $(".eltdf-logo-wrapper a").attr("href", "http://uwosh.edu/");
+
+    // Toggle for the UWO Top Navigation
+    $(".big-menu").hover(function(){
+      $(".dropdown-content").fadeToggle("medium");
+    });
+
+    // Google Custom Search Customizing
+    $(".eltdf-search-field").attr("name", "q");
+    $(".eltdf-form-holder .eltdf-column-left").prepend("<input type='hidden' name='cx' value='007334596040304417000:mkxz06xsklq' /><input type='hidden' name='ie' value='UTF-8' />");
+    $(".eltdf-search-submit").attr("name", "sa");
+    $(".eltdf-search-menu-holder").attr("action", "http://" + document.domain + "/search-results");
+  });
+
+  $( window ).load(function() {
+
+  });
+}(jQuery));
