991e5d138a4b6f565e8472c0a38b7e78949a6101
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-08-08 14:48:33 -0500

    hotfix to properly show logos on multisite

--- readanddigest-child/script.js
+++ readanddigest-child/script.js
@@ -15,10 +15,14 @@
     $(".eltdf-search-field").attr("name", "q");
     $(".eltdf-form-holder .eltdf-column-left").prepend("<input type='hidden' name='cx' value='007334596040304417000:mkxz06xsklq' /><input type='hidden' name='ie' value='UTF-8' />");
     $(".eltdf-search-submit").attr("name", "sa");
-    $(".eltdf-search-menu-holder").attr("action", "http://" + document.domain + "/search-results");
+    $(".eltdf-search-menu-holder").attr("action", "https://" + document.domain + "/search-results");
   });
 
   $( window ).load(function() {
-
+    // Hotfix to modify to src file of the Engage header images
+    // 15 in this case is the WordPress ID of the site in the multisite network
+    $(".eltdf-transparent-logo, .eltdf-light-logo, .eltdf-dark-logo, .eltdf-normal-logo").attr("src","/wp-content/uploads/sites/15/UWO-Wordmark-Reverse.png");
+    $(".eltdf-image-widget img").attr("src", "/wp-content/uploads/sites/15/ENGAGE_light.png");
+    $(".eltdf-logo-wrapper a img").attr("src", "/wp-content/uploads/sites/15/UWO-Wordmark-Reverse.png")
   });
 }(jQuery));
